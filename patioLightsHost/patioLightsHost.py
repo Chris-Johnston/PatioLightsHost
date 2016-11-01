e1e687c3048846b751985ed31309f34bccc991b5
Chris Johnston <allcapson@gmail.com>
2016-10-31 19:09:49 -0700

    additional work for web server, temp configuration for halloween

--- patioLightsHost/patioLightsHost.py
+++ patioLightsHost/patioLightsHost.py
@@ -1,26 +1,34 @@
-from GlobalConfiguration import GlobalConfiguration
+﻿from GlobalConfiguration import GlobalConfiguration
 import serial
 import time
-import random
+#import random
 from WifiBulb import WifiBulb
 from SerialLightController import SerialLightController
 
 ser = serial.Serial()
-
+# placeholder configuration for halloween 
 # patio lights main script
 config = GlobalConfiguration("configuration.ini")
+
+def cycleLights(color1, color2, longerDelay, delay):
+    time.sleep(longerDelay)
+    bulb1.setColor(color2)
+    time.sleep(delay)
+    bulb1.setColor(color1)
+    bulb2.setColor(color2)
+    time.sleep(delay)
+    bulb2.setColor(color1)
+    bulb3.setColor(color2)
+    time.sleep(delay)
+    bulb3.setColor(color1)
+
 
 if __name__ == "__main__":
     print("Start of program")
     config = GlobalConfiguration("configuration.ini")
     config.load()
-    
     # configuration loaded
     # open serial connection
-    #try:
-    #    ser = serial.Serial(config.SerialPort, config.BaudRate, timeout = 1)
-    #except Exception:
-    #    print("Error opening serial connection! (Check configuration.ini)")
     serLights = SerialLightController("configuration.ini")
     serLights.connect()
         
@@ -34,52 +42,38 @@ if __name__ == "__main__":
     bulb3 = WifiBulb(config.Lightbulb3_IP)
 
     try:
-        #bulb1.connect()
+        bulb1.connect()
         bulb2.connect()
-        #bulb3.connect()
+        bulb3.connect()
     except Exception as e:
         print("Failed to connect to one or more lightbulbs! " + str(e))
 
     try:
+        color1 = (255,0,0)
+        color2 = (0,0,0)
+        bulb1.setColor(color1)
+        bulb2.setColor(color1)
+        bulb3.setColor(color1)
+        delay = 1
+        longerDelay = 0.5
+        serLights.setPatternAndColors('2', (255,0,0), (0,0,0), 500,0)
+
         while(True):
             # read the contents of the file again
-            try:
-                file = open(config.DataFile, "rb")
-            except FileNotFoundError:
-                print("Web data file not found!")
-            # placeholder operation, just pulses the first bulb on and off
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-            #cmdStr = getSerialCommand('0', color, (0, 0, 0), 40, 0)
-            #cmdBytes = bytes(cmdStr, 'utf-8')
-            serLights.setPatternAndColors('4', color, (0,0,0), 400, 0)
-            #if (ser.isOpen() == False):
-            #    ser.open()
-            #ser.write(cmdBytes)
-            #ser.flush()
-            #ser.close()
-            #bulb1.setColor(color)
-            bulb2.setColor(color)
-            #bulb3.setColor(color)
-            time.sleep(1)
-            #cmdStr = getSerialCommand('0', (0,0,0), (0, 0, 0), 40, 0)
-            #cmdBytes = bytes(cmdStr, 'utf-8')
-            #ser.open()
-            #ser.write(cmdBytes)
-            #ser.flush()
-            #ser.close()
-            #bulb1.setColor((0,0,0))
-            #bulb2.setColor((0,0,0))
-            #serLights.setPatternAndColors('0', (0,0,0), (0,0,0), 40, 0)
-            #bulb3.setColor((0,0,0))
-            #time.sleep(0.5)
+            #try:
+            #    file = open(config.DataFile, "rb")
+            #except FileNotFoundError:
+            #    print("Web data file not found!")
+            
+            # keep the lights on for a few seconds
+            #placeholder pattern, just turn the lights on and off in a pulsing pattern
+            cycleLights(color1, color2, longerDelay, delay)
     except(KeyboardInterrupt, SystemExit):
         print("End of program")
         # turn off the bulbs
-        #bulb1.setColor((0,0,0))
-        #bulb1.disconnect()
+        bulb1.disconnect()
         bulb2.disconnect()
-        #bulb3.setColor((0,0,0))
-        #bulb3.disconnect()
+        bulb3.disconnect()
         serLights.disconnect()
         print("end")
         #turn everything off
---
+++ patioLightsHost/www/submit.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Patio Lights Color Picker</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <!-- uses jscolor for color picker jscolor.com -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.js"></script>
+    <!-- Google Web Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Oswald|Roboto+Condensed:400,400italic,700italic,700,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="header">
+        <h1>Patio Lights Color Picker</h1>
+        <a id="githublink" href="https://github.com/chris-johnston">github.com/Chris-Johnston</a>
+    </div>
+    <div id="wrapper">
+        <div id="inner">
+            <div id="status">
+                Your information has been entered.
+                <br />
+                Data:
+                <?php 
+                // open the colorData file so we can write to it
+                $colorDataFile = fopen("colorData", "w+") or die("File error");
+                // get each of the parameters from the form
+                $color1 = $_GET["color1"];
+                $color2 = $_GET["color2"];
+                $bulb1 = $_GET["bulb1"];
+                $bulb2 = $_GET["bulb2"];
+                $bulb3 = $_GET["bulb3"];
+                $stripPattern = $_GET["stripPattern"];
+                $bulbPattern = $_GET["bulbPattern"];
+                $stripDelay1 = $_GET["stripDelay1"];
+                $bulbDelay1 = $_GET["bulbDelay1"];
+
+                function hexToRGB($hexColorValue)
+                {
+                    // remove any # from the hex value
+                    $hex = str_replace("#", "", $hexColorValue);
+                    if(strlen($hex) == 3) {
+                        $_r = hexdec(substr($hex,0,1).substr($hex,0,1));
+                        $_g = hexdec(substr($hex,1,1).substr($hex,1,1));
+                        $_b = hexdec(substr($hex,2,1).substr($hex,2,1));
+                    } else {
+                        $_r = hexdec(substr($hex,0,2));
+                        $_g = hexdec(substr($hex,2,2));
+                        $_b = hexdec(substr($hex,4,2));
+                    }
+                    $rgb = array($_r, $_g, $_b);
+                    //return implode(",", $rgb); // returns the rgb values separated by commas
+                    return $rgb; // returns an array with the rgb values
+                }
+
+                $stripColor1 = hexToRGB($color1);
+                $stripColor2 = hexToRGB($color2);
+                $bulbColor1 = hexToRGB($bulb1);
+                $bulbColor2 = hexToRGB($bulb2);
+                $bulbColor3 = hexToRGB($bulb3);
+
+                echo $stripColor1;
+                echo $stripColor2;
+                echo $bulbColor1;
+                echo $bulbColor2;
+                echo $bulbColor3;
+
+                ?>
+            </div>
+            <br />
+            <a href="index.html">Return the the color picker.</a>
+        </div>
+    </div>
+</body>
+</html>
